b022425bead93e51e0d80a1239c0ab085486b99a
jazz <jazxbx@gmail.com>
2023-08-24 12:58:57 -0700

    added index.js file

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,23 @@
+// import { petsData } from "../lib";
+
 export default function Card(props) {
+  const pet = props.pet;
+
   return (
-    <div id="pet-container">
-      <div id="pet-emoji">{props.pet.icon}</div>
-      <h2>{props.pet.name}</h2>
-      <p className="hunger-meter">Hunger</p>
+    <div id="pet-card" className="card-style">
+      <div id="pet-emoji">{pet.icon}</div>
+      <h2>{pet.name}</h2>
+      <p className="hunger-meter">Hunger:</p>
       <meter
         className="hunger-indicator"
-        value={props.pet.hunger}
+        value={pet.hunger}
         max="100"
         min="0"
       ></meter>
+      <p>Love:</p>
       <meter
         className="love-indicator"
-        value={props.pet.love}
+        value={pet.love}
         max="100"
         min="0"
       ></meter>
---
+++ src/components/FeedPetBtn.jsx
@@ -0,0 +1,1 @@
+// Whenever this feed btn is pressed, it clears pets hunger
